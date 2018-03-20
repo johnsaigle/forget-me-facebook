261d75fa1d7b5f07f680b7453298f289a85b78d4
John Saigle <john@computer.local>
2018-03-20 00:05:08 -0400

    Raise exception if casperjs is not in local dir

--- forget.py
+++ forget.py
@@ -2,6 +2,7 @@
 """Periodically forget Facebook interaction data."""
 
 import subprocess
+import sys
 import getpass
 
 def forget():
@@ -10,6 +11,9 @@ def forget():
         subprocess.call(['node_modules/casperjs/bin/casperjs', 'first-commit.js'])
     except KeyboardInterrupt:
         print("Got CTRL-C. Exiting.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("Couldn't find node_modules/casperjs/bin/casperjs in local directory.")
         sys.exit(1)
 
 if __name__ == '__main__':
