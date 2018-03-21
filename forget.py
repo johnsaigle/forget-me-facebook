130a8bb8e66ce4b478a47fa90484afb79ab8567b
John Saigle <john@computer.local>
2018-03-21 12:51:45 -0400

    Use scrapy to login

--- forget.py
+++ forget.py
@@ -3,18 +3,29 @@
 
 import subprocess
 import sys
-import getpass
 
-def forget():
+import scrapy
+
+def use_casperjs():
     """Launch casper script to access Facebook"""
     try:
         subprocess.call(['node_modules/casperjs/bin/casperjs', 'first-commit.js'])
-    except KeyboardInterrupt:
-        print("Got CTRL-C. Exiting.")
-        sys.exit(1)
     except FileNotFoundError:
         print("Couldn't find node_modules/casperjs/bin/casperjs in local directory.")
         sys.exit(1)
 
+def get_facebook_session_token(email, password):
+    """Submit credentials to Facebook's login and retrive the session token."""
+    url = 'https://facebook.com'
+    my_data = {'email': email, 'pass': password}
+    request = scrapy.Request(url, method='POST',
+                          body=json.dumps(my_data),
+                          headers={'Content-Type':'application/json'})
+
+
 if __name__ == '__main__':
-    forget()
+    try:
+        forget()
+    except KeyboardInterrupt:
+        print("Exiting.")
+        sys.exit(1)
---
+++ forget/forget/spiders/facebook_spider.py
@@ -0,0 +1,38 @@
+import scrapy
+import getpass
+
+def prompt_for_credentials():
+    """Prompt for user email and password on the command-line"""
+    print("Enter email:")
+    email = input()
+    print("Enter password:")
+    password = getpass.getpass()
+    credentials = (email, password)
+    return credentials
+
+class LoginSpider(scrapy.Spider):
+    """Submits login credentials to Facebook"""
+    name = "login"
+
+    def start_requests(self):
+        url = 'https://facebook.com/login.php'
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        credentials = prompt_for_credentials()
+        email = credentials[0]
+        password = credentials[1]
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'email': email, 'pass': password},  # TODO: prompt for password
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        """Steps to do after successful login"""
+        # make sure login was successful
+        if b'Sign up for Facebook' in response.body:
+            self.logger.error("Login failed")
+            return
+
+        self.logger.info("Login success.")
